1fadc730ef84b011e7cd3104cb819fdef0199eee
hvinzant <hugues.vinzant@epfl.ch>
2018-11-05 16:42:12 +0100

    Changes in cross validation and graphs

--- matlab/DAMC_W4_PCA.m
+++ matlab/DAMC_W4_PCA.m
@@ -10,7 +10,7 @@ load('testSet.mat');
 
 %% PCA
 
-[outter_coeff, score, variance] = pca(trainData);
+[outter_coeff, score, variance] = pca(trainData(:,1:10:end));
 %Covariance matrix of the original data 
 Cov = cov(trainData);
 %covariance matrix of the data projected on the PCs
@@ -18,7 +18,7 @@ Cov_PCA = cov(score);
 
 figure(1)
 subplot(1,2,1),
-imshow(Cov * 100), %=> the image is too large to apply this function
+imshow(Cov * 100),
 title('Covariance of the raw data')
 subplot(1,2,2),
 imshow(Cov_PCA * 100),
@@ -30,6 +30,7 @@ title('Covariance of the PCA data')
 % value = max(max(modified_cov));
 % value_pca = max(max(modified_cov_PCA));
 
+variance = variance/sum(variance) * 100;
 variance_cum = cumsum(variance);
 
 figure(2)
@@ -39,7 +40,7 @@ xlabel('Feature'), ylabel('Variance %')
 
 %% 
 
-outer_folds = 2;
+outer_folds = 10;
 inner_folds = 5;
 
 outer_cvpartition = cvpartition(trainLabels,'kfold',outer_folds);
@@ -50,8 +51,8 @@ for i = 1:outer_folds
     waitbar(i/outer_folds)
 
     outer_train_labels = trainLabels(outer_indices == 0);
-    outer_train_data = trainData(outer_indices == 0,:);
-    outer_test_data = trainData(outer_indices == 1,:);
+    outer_train_data = trainData(outer_indices == 0,1:10:end);
+    outer_test_data = trainData(outer_indices == 1,1:10:end);
     outer_test_labels = trainLabels(outer_indices == 1);
     
     inner_cvpartition = cvpartition(outer_train_labels,'kfold',inner_folds);
@@ -64,27 +65,21 @@ for i = 1:outer_folds
         inner_test_data = outer_train_data(inner_indices == 1,:);
         inner_test_labels = outer_train_labels(inner_indices == 1);
         
-        [inner_train_data, mu, sigma] = zscore(inner_train_data, 0, 1);
-        inner_test_data = (inner_test_data - mu)./sigma;
-        
-        inner_coeff = pca(inner_train_data);
+        [inner_coeff, inner_score, ~, ~, explained_var, mu] = pca(inner_train_data);
         inner_PCA_data = inner_train_data * inner_coeff;
-        inner_PCA_data_te = inner_test_data * inner_coeff;
+        inner_PCA_data_te = (inner_test_data - mu) * inner_coeff;
+
+        %tot_inner_variance(j,:) = cumsum(explained_var(1:200));
         
-        for N_sel = 1:(length(inner_PCA_data(1,:)))
+        for N_sel = 1:length(explained_var)
             
             train_data_sel = inner_PCA_data(:,1:N_sel);
             val_data_sel = inner_PCA_data_te(:,1:N_sel);
             
-            inner_classifier = fitcdiscr(train_data_sel, inner_train_labels, 'discrimtype', 'diagquadratic');
-
-            inner_label_prediction = predict(inner_classifier, train_data_sel);
+            inner_classifier = fitcdiscr(train_data_sel, inner_train_labels, 'discrimtype', 'diaglinear');
             inner_label_prediction_val = predict(inner_classifier, val_data_sel);
 
-            classification_error = classification_errors(inner_train_labels, inner_label_prediction);
             classification_error_val = classification_errors(inner_test_labels, inner_label_prediction_val);
-            
-            error(j,N_sel) = classification_error;
             error_val(j,N_sel) = classification_error_val;
         end
     end
@@ -93,21 +88,23 @@ for i = 1:outer_folds
     mean_error = mean(error_val, 1);
     [min_error(i), best_N(i)] = min(mean_error);
     
+%     mean_var = mean(tot_inner_variance, 1);
+%     best_var(i) = mean_var(best_N(i));
+    
     [outer_train_data, mu, sigma] = zscore(outer_train_data, 0, 1);
     outer_test_data = (outer_test_data - mu)./sigma;
 
-    outer_coeff = pca(outer_train_data);
+    [outer_coeff, ~, ~, ~, ~, mu] = pca(outer_train_data);
     outer_PCA_data = outer_train_data * outer_coeff;
-    outer_PCA_data_te = outer_test_data * outer_coeff;
+    outer_PCA_data_te = (outer_test_data - mu) * outer_coeff;
     
     outer_train_data_sel = outer_PCA_data(:,1:best_N(i));
     outer_test_data_sel = outer_PCA_data_te(:,1:best_N(i));
-    outer_classifier = fitcdiscr(outer_train_data_sel, outer_train_labels, 'discrimtype', 'diagquadratic');
+    outer_classifier = fitcdiscr(outer_train_data_sel, outer_train_labels, 'discrimtype', 'diaglinear');
 
     outer_label_prediction = predict(outer_classifier, outer_test_data_sel);
 
     classification_error_test(i) = classification_errors(outer_test_labels, outer_label_prediction);
-    
 end
 
 
