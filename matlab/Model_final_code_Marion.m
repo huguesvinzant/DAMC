8bc19caac00b80b3c9299d2c1bb938aed4f31e73
hvinzant <hugues.vinzant@epfl.ch>
2018-11-06 14:10:30 +0100

    final model modifs

--- matlab/Model_final_code_Marion.m
+++ matlab/Model_final_code_Marion.m
@@ -11,31 +11,21 @@ load('testSet.mat');
 %% PCA
 
 [coeff, PCA_data,  ~, ~, explained_var, mu] = pca(trainData);
-
-
-%%%
-%needed ?
-% variance = variance/sum(variance) * 100;
-% variance_cum = cumsum(variance);
+PCA_data_te = (testData - mu) * coeff;
 
 %% Normalization ??
 
 %% CV for hyperparameters optimization
 
-%k = 10;
-k = 2;
-N_sel = 500;
-%Classifiers = ['linear       '; 'diaglinear   '; 'diagQuadratic'];
-%space characters are needed to obtain the same element' size in the char vector 
-
+k = 5;
+N_sel = 596;
 Classifiers = {'linear', 'diaglinear', 'diagQuadratic'};
 
 cvpartition_ = cvpartition(trainLabels,'kfold',k);
 
-%for i = 1:k
-    %waitbar(i/k)
-    %idxcv = test(cvpartition_,i); % 0 is for training, 1 is of testing
-    idxcv = test(cvpartition_,1);
+for i = 1:k
+    waitbar(i/k)
+    idxcv = test(cvpartition_,i); % 0 is for training, 1 is of testing
     
 %   we use the indices of the cv partition to create a train and a test
 %   set from the data obtained after PCA
@@ -50,35 +40,44 @@ cvpartition_ = cvpartition(trainLabels,'kfold',k);
 %   orderedPower: Sorted power of features
     [orderedInd, orderedPower] = rankfeat(train_data, train_labels, 'fisher');
 
-    for m = 1:size(Classifiers,1)
+    for m = 1:length(Classifiers)
         for j = 1:N_sel
             train_data_sel = train_data(:,orderedInd(1:j));
             test_data_sel = test_data(:,orderedInd(1:j));
             
             classifier = fitcdiscr(train_data_sel, train_labels, 'discrimtype', Classifiers{m});
-            %classifier = fitcdiscr(train_data_sel, train_labels, 'discrimtype', 'linear');
             
             label_prediction = predict(classifier, train_data_sel);
             label_prediction_test = predict(classifier, test_data_sel);
 
             class_error = classification_errors(train_labels, label_prediction);
             class_error_test = classification_errors(test_labels, label_prediction_test);
-
-            error_for_a_classifier(j) = class_error;
-            error_test_for_a_classifier(j) = class_error_test;
- 
-        end
-        %Iteration sur les j pour un m fixé = 1 classifier fixé afin de
-        %trouver l'erreur min
-        
-        %[M,I] = min(A)=> M min / A = indice
-        [smallest_error_for_a_classifier, N_for_smallest_error_for_a_classifier] = min(error_test_for_a_classifier');
-        smallest_error_bestN_per_classifier(m,1) = smallest_error_for_a_classifier;
-        smallest_error_bestN_per_classifier(m,2) = N_for_smallest_error_for_a_classifier;       
+            
+            if m == 1
+                linear_error(i,j) = class_error;
+                linear_error_te(i,j) = class_error_test;
+            end
+            if m == 2
+                diaglinear_error(i,j) = class_error;
+                diaglinear_error_te(i,j) = class_error_test;
+            end
+            if m == 3
+                diagquadratic_error(i,j) = class_error;
+                diagquadratic_error_te(i,j) = class_error_test;
+            end
+        end    
     end
-%end
-[smalest_error, Classifier_with_smalest_error] = min(smallest_error_bestN_per_classifier(:,1));
-N_associated_with_Classifier_with_smallest_error = smallest_error_bestN_per_classifier(Classifier_with_smalest_error,2);
+end
+
+[min_errors(1), best_Ns(1)] = min(mean(linear_error_te, 1));
+[min_errors(2), best_Ns(2)] = min(mean(diaglinear_error_te, 1));
+[min_errors(3), best_Ns(3)] = min(mean(diagquadratic_error_te, 1));
+
+figure(1)
+subplot(1,3,1), plot(mean(linear_error, 1)), hold on, plot(mean(linear_error_te, 1))
+subplot(1,3,2), plot(mean(diaglinear_error, 1)), hold on, plot(mean(diaglinear_error_te, 1))
+subplot(1,3,3), plot(mean(diagquadratic_error, 1)), hold on, plot(mean(diagquadratic_error_te, 1))
+
 
 %% Nested CV
 
@@ -86,8 +85,9 @@ N_associated_with_Classifier_with_smallest_error = smallest_error_bestN_per_clas
 
 %% Retraining of our optimized model on the whole data set
 
-Best_classifier = Classifiers{Classifier_with_smalest_error};
-Best_N = N_associated_with_Classifier_with_smallest_error;
+[min_error, best_class_idx] = min(min_errors);
+Best_classifier = Classifiers{best_class_idx};
+Best_N = best_Ns(best_class_idx);
 
 %No more separation in a test and a train set
 [orderedInd_final, orderedPower_final] = rankfeat(PCA_data, trainLabels, 'fisher');
@@ -95,8 +95,7 @@ Best_N = N_associated_with_Classifier_with_smallest_error;
 train_final = PCA_data(:,orderedInd_final(1:Best_N));
 classifier_final = fitcdiscr(train_final, trainLabels, 'discrimtype', Best_classifier);
 
-PCA_data_test = (testData - mu) * coeff;
-test_final = PCA_data_test(:,orderedInd_final(1:Best_N));
+test_final = PCA_data_te(:,orderedInd_final(1:Best_N));
 
 label_prediction_final = predict(classifier_final, test_final);
 
